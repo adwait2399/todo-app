82cff10250b8d0fb95f251156414442f6366843d
adwait <adwaitsupekar@gmail.com>
2021-01-06 18:09:53 +0530

    function added 2

--- app.js
+++ app.js
@@ -3,11 +3,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 //event listeners
 
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deletecheck);
+filterOption.addEventListener('click', filterTodo);
 
 //functions
 
